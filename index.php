b491ee0ae8eff43086ce4e11eadfde4982dde132
MuffinzCat <33560694+MuffinzCat@users.noreply.github.com>
2021-07-12 16:37:47 +0200

    added main window, button, button sounds, class change on-hover and css

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>TF2 Loadout Randomiser</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+
+<div class="center">
+    <h1>Loadout Randomiser!</h1>
+    <button id="randomiseButton" onclick="buttonClick()" onmouseover="randomiseButtonHover()" onmouseleave="randomiseButtonUnHover()">Randomise!</button>
+</div>
+
+
+<script src="index.js"></script>
+</body>
+</html>
+
+<?php
+
